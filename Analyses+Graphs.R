c65728ffb0d278f6d31b020628c0b222a418e9b5
Ryo Nakagawara <ryonakagawara@gmail.com>
2017-05-03 23:43:39 +0100

    - graphs cont.

--- Analyses+Graphs.R
+++ Analyses+Graphs.R
@@ -23,8 +23,14 @@ JapanCamera
 
 JapanCamera %>% ggplot(aes(x = Year, y = value, group = variable)) + geom_line()
 
-JapanCamera %>% ggplot(aes(x = Year, y = Japanese.Camera.Production)) + geom_line() +
-  geom_line(aes(y = USA.Import.of.Japanese.Cameras))
+JapanCamera %>% ggplot(aes(x = Year, y = Japanese.Camera.Production)) + 
+  geom_line(size = 2) + geom_point(aes(y = Japanese.Camera.Production), color = "blue") +
+  geom_line(aes(y = USA.Import.of.Japanese.Cameras), color = I("red"), size = 2) + geom_point(aes(y = USA.Import.of.Japanese.Cameras)) +
+  scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
+  scale_y_continuous(breaks = pretty_breaks(n = 6)) +
+  theme_economist() +
+  theme(axis.title = element_text(size = 15)) +
+  labs(x = "Year", y = "Camera Production (Units)", title = "Japan Camera Production and USA Imports of Japanese Cameras\n (1945-1960)")
 
 
 JapanCotton <- read.csv("~/R materials/AmericaJapanPostWW2Trade/Data/JapanCottonTextileExports(1946-1960).csv")
