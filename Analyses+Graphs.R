627b16963236dc46b19ee47da66f94f95addf4c6
Ryo-N7 <ryonakagawara@gmail.com>
2017-05-08 11:25:39 +0100

    - graphs update

--- Analyses+Graphs.R
+++ Analyses+Graphs.R
@@ -8,7 +8,7 @@ steelprod <- read.csv("~/R materials/AmericaJapanPostWW2Trade/Data/JapanSteelPro
 steelprod
 
 steelprod %>% ggplot(aes(x = Year, y = Steel.production)) + geom_line(size = 2.5) +
-  theme_economist() +
+  theme_economist_white() +
   theme(axis.text = element_text(size = 10), axis.title = element_text(size = 15)) +
   scale_x_continuous(breaks = pretty_breaks(n = 10)) +
   scale_y_continuous(breaks = pretty_breaks(n = 10)) +
@@ -28,10 +28,21 @@ JapanCamera %>% ggplot(aes(x = Year, y = Japanese.Camera.Production)) +
   geom_line(aes(y = USA.Import.of.Japanese.Cameras), color = I("red"), size = 2) + geom_point(aes(y = USA.Import.of.Japanese.Cameras)) +
   scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
   scale_y_continuous(breaks = pretty_breaks(n = 6)) +
-  theme_economist() +
-  theme(axis.title = element_text(size = 15)) +
+  theme_wsj() +
+  theme(axis.title = element_text(size = 10), axis.text = element_text(size = 7), title = element_text(size = 7.5)) +
   labs(x = "Year", y = "Camera Production (Units)", title = "Japan Camera Production and USA Imports of Japanese Cameras\n (1945-1960)")
 
 
 JapanCotton <- read.csv("~/R materials/AmericaJapanPostWW2Trade/Data/JapanCottonTextileExports(1946-1960).csv")
-JapanCotton
+
+JapanCotton %>% ggplot(aes(Year, y = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.), color = I("blue")) +
+  geom_line(size = 2) + geom_line(aes(y = Cotton.Textile.Exports.to.USA..million.square.yards.))
+
+
+
+
+
+
+
+
+
