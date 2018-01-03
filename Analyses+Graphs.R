264639f06cad440fcb6a30d37d3d7321b03b73dc
Ryo-N7 <ryonakagawara@gmail.com>
2018-01-03 01:11:53 +0000

    - start article with gganimate!

--- Analyses+Graphs.R
+++ Analyses+Graphs.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(ggthemes)
 library(scales)
 
-steelprod <- read.csv("~/R materials/AmericaJapanPostWW2Trade/Data/JapanSteelProduction(1950-1960).csv")
+steelprod <- read.csv("~/R_materials/AmericaJapanPostWW2Trade/Data/JapanSteelProduction(1950-1960).csv")
 steelprod
 
 steelprod %>% ggplot(aes(x = Year, y = Steel.production)) + 
@@ -18,6 +18,21 @@ steelprod %>% ggplot(aes(x = Year, y = Steel.production)) +
        title = "Japan Steel Production\n(1950-1960)") 
 
 
+library(gganimate)
+
+steel_animate <- steelprod %>% 
+  ggplot(aes(x = Year, y = Steel.production, frame = Year, cumulative = TRUE)) + 
+  geom_line(size = 2.5) +
+  theme_economist_white() +
+  theme(axis.text = element_text(size = 10), axis.title = element_text(size = 15),
+        plot.title = element_text(hjust = 0.5)) +
+  scale_x_continuous(breaks = pretty_breaks(n = 10)) +
+  scale_y_continuous(breaks = pretty_breaks(n = 10), label = comma) +
+  labs(x = "Year", y = "Steel Production (Units)", 
+       title = "Japan Steel Production\n(1950-1960)") 
+
+gganimate(steel_animate)
+
 ?scales::trans_new
 
 
@@ -25,7 +40,7 @@ steelprod %>% ggplot(aes(x = Year, y = Steel.production)) +
 #
 
 
-JapanCamera <- read.csv("~/R materials/AmericaJapanPostWW2Trade/Data/JapanCamera&USAImportCamera(1945-1960).csv")
+JapanCamera <- read.csv("~/R_materials/AmericaJapanPostWW2Trade/Data/JapanCamera&USAImportCamera(1945-1960).csv")
 JapanCamera
 
 JapanCamera %>% ggplot(aes(x = Year, y = value, group = variable)) + geom_line()
@@ -33,7 +48,7 @@ JapanCamera %>% ggplot(aes(x = Year, y = value, group = variable)) + geom_line()
 JapanCamera %>% ggplot(aes(x = Year, y = Japanese.Camera.Production)) + 
   geom_line(size = 2) + 
   geom_point(aes(y = Japanese.Camera.Production), color = "red") +
-  geom_line(aes(y = USA.Import.of.Japanese.Cameras), color = I("red"), size = 2) +
+  geom_line(aes(y = USA.Import.of.Japanese.Cameras), color = "red", size = 2) +
   geom_point(aes(y = USA.Import.of.Japanese.Cameras)) +
   scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
   scale_y_continuous(breaks = pretty_breaks(n = 6), labels = comma) +
@@ -46,21 +61,46 @@ JapanCamera %>% ggplot(aes(x = Year, y = Japanese.Camera.Production)) +
        title = "Japan Camera Production and USA Imports of Japan Cameras\n (1945-1960)")
 
 
+# animate:
+
+camera_animate <- JapanCamera %>% 
+  ggplot(aes(x = Year, y = Japanese.Camera.Production, frame = Year, cumulative = TRUE)) + 
+  geom_line(size = 2) + 
+  geom_point(aes(y = Japanese.Camera.Production), color = "red") +
+  geom_line(aes(y = USA.Import.of.Japanese.Cameras), color = "red", size = 2) +
+  geom_point(aes(y = USA.Import.of.Japanese.Cameras)) +
+  scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
+  scale_y_continuous(breaks = pretty_breaks(n = 6), labels = comma) +
+  theme_wsj() +
+  theme(axis.title = element_text(size = 10),
+        axis.text = element_text(size = 7), 
+        title = element_text(size = 7.5),
+        plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Year", y = "Camera Production (Units)", 
+       title = "Japan Camera Production and USA Imports of Japan Cameras\n (1945-1960)")
+
+
+gganimate(camera_animate)
+
 
 #
 
 
 
-JapanCotton <- read.csv("~/R materials/AmericaJapanPostWW2Trade/Data/JapanCottonTextileExports(1946-1960).csv")
+JapanCotton <- read.csv("~/R_materials/AmericaJapanPostWW2Trade/Data/JapanCottonTextileExports(1946-1960).csv")
 
 JapanCotton
 
-JapanCotton %>% ggplot(aes(Year, y = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.), color = I("blue")) +
-  geom_line(size = 2) + geom_line(aes(y = Cotton.Textile.Exports.to.USA..million.square.yards.), size = 2)
+JapanCotton %>% 
+  ggplot(aes(Year, y = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.), color = I("blue")) +
+  geom_line(size = 2) + 
+  geom_line(aes(y = Cotton.Textile.Exports.to.USA..million.square.yards.), size = 2)
 
-JapanCotton %>% rename(Cotton.From.Japan.Exports = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.,
-                       All.USA.Exports = Cotton.Textile.Exports.to.USA..million.square.yards.) %>% 
-  ggplot(aes(Year, y = Cotton.From.Japan.Exports)) + geom_line(size = 1.5) +
+JapanCotton %>% 
+  rename(Cotton.From.Japan.Exports = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.,
+         All.USA.Exports = Cotton.Textile.Exports.to.USA..million.square.yards.) %>% 
+  ggplot(aes(Year, y = Cotton.From.Japan.Exports)) + 
+  geom_line(size = 1.5) +
   geom_line(aes(y = All.USA.Exports)) +
   scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
   scale_y_continuous(breaks = pretty_breaks(n = 6), labels = comma) +
@@ -74,10 +114,12 @@ JapanCotton %>% rename(Cotton.From.Japan.Exports = Japan.Cotton.Textile.Exports.
 
 
 
-JapanCotton %>% rename(Cotton.From.Japan.Exports = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.,
-                       All.USA.Exports = Cotton.Textile.Exports.to.USA..million.square.yards.) %>% 
+JapanCotton %>%
+  rename(Cotton.From.Japan.Exports = Japan.Cotton.Textile.Exports.to.USA..million.square.yards.,
+         All.USA.Exports = Cotton.Textile.Exports.to.USA..million.square.yards.) %>% 
   ggplot(aes(Year, y = All.USA.Exports)) + 
-  geom_line(size = 1.5) + geom_point(color = I("red"), size = 3) +
+  geom_line(size = 1.5) + 
+  geom_point(color = I("red"), size = 3) +
   scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
   scale_y_continuous(breaks = pretty_breaks(n = 6), labels = comma) +
   theme_wsj() +
