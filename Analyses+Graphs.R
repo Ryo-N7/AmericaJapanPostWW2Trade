00449ea0964d3439907e36693b805044f1265806
Ryo-N7 <ryonakagawara@gmail.com>
2018-01-10 02:02:03 +0000

    - create tweenr camera, writeup continue

--- Analyses+Graphs.R
+++ Analyses+Graphs.R
@@ -78,22 +78,19 @@ JapanCamera %>% ggplot(aes(x = Year, y = Japanese.Camera.Production)) +
 
 # animate:
 
-camera_animate <- JapanCamera %>% 
-  ggplot(aes(x = Year, y = Japanese.Camera.Production, frame = Year, cumulative = TRUE)) + 
-  geom_line(size = 2) + 
-  geom_point(aes(y = Japanese.Camera.Production), color = "red") +
-  geom_line(aes(y = USA.Import.of.Japanese.Cameras), color = "red", size = 2) +
-  geom_point(aes(y = USA.Import.of.Japanese.Cameras)) +
+camera_animate <- Japan_Camera %>% 
+  ggplot(aes(x = Year, y = Camera_Production, frame = Year, cumulative = TRUE)) + 
+  geom_path(aes(y = Camera_Production), color = "#474747", size = 1.5) +
+  geom_path(aes(y = USA_Imports), color = "black", size = 1.5) +
   scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
-  scale_y_continuous(breaks = pretty_breaks(n = 6), labels = comma) +
-  theme_wsj() +
+  scale_y_continuous(labels = c("0", "500,000", "1 Million", "1.5 Million", "2 Million")) +
+  theme_bw() +
   theme(axis.title = element_text(size = 10),
-        axis.text = element_text(size = 7), 
-        title = element_text(size = 7.5),
+        axis.text = element_text(size = 8), 
+        title = element_text(size = 9.5),
         plot.title = element_text(hjust = 0.5)) +
-  labs(x = "Year", y = "Camera Production (Units)", 
-       title = "Japan Camera Production and USA Imports of Japan Cameras\n (1945-1960)")
-
+  labs(x = "Year", y = "Japanese Cameras (Units)", 
+       title = "Japanese Camera Production and USA Imports of Japanese Cameras\n (1945-1960)")
 
 gganimate(camera_animate)
 
@@ -144,6 +141,202 @@ JapanCotton %>%
         plot.title = element_text(hjust = 0.5)) +
   labs(x = "Year", y = "Cotton Production (Million Yards)", 
        title = "USA Total Cotton Imports \n (1945-1960)")
+
+
+
+
+
+
+
+
+
+
+
+# tweenR ------------------------------------------------------------------
+library(dplyr) # pipes
+library(readr)    # reading in .csv files
+
+library(ggplot2)  # plotting
+library(ggthemes) # ggplot2 themes
+library(scales)
+
+
+Japan_Camera <- read_csv("Data/JapanCamera&USAImportCamera(1945-1960).csv",
+                         col_names = c("Year", "Camera_Production", "USA_Imports"),
+                         skip = 1)
+
+library(tweenr)
+
+glimpse(Japan_Camera)
+
+Japan_Camera_tween <- Japan_Camera %>% 
+  rename(y = Camera_Production, time = Year) %>% 
+  mutate(id = time, 
+         x = time,
+         ease = "linear") %>% 
+  select(x, y, time, id, ease)
+
+glimpse(Japan_Camera_tween)
+
+Japan_Camera_tween <- Japan_Camera_tween %>% 
+  mutate(id = as.factor(id))
+
+Japan_cam_tween <- tween_elements(Japan_Camera_tween, "time", "id", "ease", nframes = 1000)
+
+glimpse(Japan_cam_tween)
+
+
+camera_animate <- Japan_Camera %>% 
+  ggplot(aes(x = Year, y = Camera_Production, frame = Year, cumulative = TRUE)) + 
+  geom_path(aes(y = Camera_Production), color = "#474747", size = 1.5) +
+  geom_path(aes(y = USA_Imports), color = "black", size = 1.5) +
+  scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
+  scale_y_continuous(labels = c("0", "500,000", "1 Million", "1.5 Million", "2 Million")) +
+  theme_bw() +
+  theme(axis.title = element_text(size = 10),
+        axis.text = element_text(size = 8), 
+        title = element_text(size = 9.5),
+        plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Year", y = "Japanese Cameras (Units)", 
+       title = "Japanese Camera Production and USA Imports of Japanese Cameras\n (1945-1960)")
+
+gganimate(camera_animate)
+
+
+
+
+# plotly ------------------------------------------------------------------
+
+library(plotly)
+
+glimpse(Japan_Camera)
+
+J_Cam <- Japan_Camera %>% 
+  ggplot(aes(x = Year)) + 
+  geom_line(aes(y = Camera_Production, color = "Camera Production", frame = Year), size = 1.5) +
+  geom_line(aes(y = USA_Imports, color = "USA Imports", frame = Year), size = 1.5) +
+  scale_x_continuous(breaks = pretty_breaks(n = 20)) + 
+  scale_y_continuous(labels = c("0", "500,000", "1 Million", "1.5 Million", "2 Million")) +
+  scale_color_manual(values = c("Camera Production" = "#474747", 
+                                "USA Imports" = "black")) +
+  geom_vline(xintercept = 1950) +
+  geom_vline(xintercept = 1953) +
+  annotate(geom = "text", 
+           label = "Korean\nWar", size = 5, color = "#474747",
+           x = 1951.5, y = 1000000) +
+  theme_bw() +
+  theme(axis.title = element_text(size = 10),
+        axis.text = element_text(size = 8), 
+        title = element_text(size = 9.5),
+        plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.15, 0.9),
+        legend.title = element_blank()) +
+  labs(x = "Year", y = "Japanese Cameras (Units)", 
+       title = "Japanese Camera Production and USA Imports of Japanese Cameras\n (1945-1960)")
+
+
+J_Cam <- Japan_Camera %>% 
+  ggplot(aes(x = Year, y = Camera_Production)) + 
+  geom_path(aes(frame = Year)) +
+  geom_point(aes(frame = Year))
+
+
+Cam_Plotly <- ggplotly(J_Cam)
+
+Cam_Plotly
+
+Sys.setenv("plotly_username" = "RN1892")
+Sys.setenv("plotly_api_key" = "M5PHBeFifVcHtfjfrFRv")
+
+chart_link <- api_create(Cam_Plotly, filename = "camera-plot")
+
+chart_link
+
+
+df <- data.frame(
+  x = c(1,2,3,4), 
+  y = c(1,2,3,4), 
+  f = c(1,2,3,4)
+)
+
+p <- ggplot(df, aes(x, y)) +
+  geom_point(aes(frame = f))
+
+ggplotly(p)
+
+J_Cam
+
+
+
+
+
+# time series tweenr  -----------------------------------------------------
+
+library(tweenr)
+library(tidyverse)
+
+data <- tribble(
+  ~year, ~num,
+  1950, 56,
+  1951, 59,
+  1952, 64,
+  1953, 67,
+  1954, 69,
+  1955, 74,
+  1956, 78,
+  1957, 83
+)
+
+dat_ani <- map(seq(nrow(data)), ~data[c(seq(.x), rep(.x, nrow(data) - .x)), ]) %>% 
+  tweenr::tween_states(5, 2, 'cubic-in-out', 100) %>% 
+  ggplot(aes(year, num, frame = .frame)) + 
+  geom_path() + 
+  geom_point()
+
+animation::ani.options(interval = 0.5)    # speed it up
+gganimate::gganimate(dat_ani, title_frame = FALSE)
+
+seq(nrow(data))
+
+# 1,2,3,4,5,6,7,8
+map(seq(nrow(data)), ~data[c(seq(.x), 
+                             rep(.x, nrow(data) - .x) 
+), 
+]
+)
+
+dt <- map(seq(nrow(data)), ~data[c(seq(.x), rep(.x, nrow(data) - .x)), ])
+
+str(dt, list.len = 2)
+
+data[[1]]
+data[[2]]
+data[2, ]
+data[1, ]
+data[1, 2]
+
+rep(1, nrow(data) - 1)
+
+rep(1, nrow(data) - 6)
+
+rep(3, nrow(data) - 3)
+
+rep(2, nrow(data) - 3)
+
+seq(data)
+str(data)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
